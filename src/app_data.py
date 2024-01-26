8722c9c96e2a9c812d369c309c382e12f18461b4
Yanpeng Wu <pengfly20@gmail.com>
2024-01-25 20:31:12 -0500

    Construct the portfolio weight time series

--- src/app_data.py
+++ src/app_data.py
@@ -43,24 +43,20 @@ def get_stats(df_return):
 
     return stats
 
-
-# Equal weight
 @st.cache_data
 def get_eqw_pf_returns(df_return):
     return df_return.mean(axis=1)[252:]
 
-# Optimal
 @st.cache_data
 def get_opt_weights(df_price):
     mu = expected_returns.mean_historical_return(df_price)
-    S = risk_models.sample_cov(df_price)
+    cov_mx = risk_models.sample_cov(df_price)
 
     # optimize for max sharpe ratio
-    ef = EfficientFrontier(mu, S)
+    ef = EfficientFrontier(mu, cov_mx)
     weights = ef.max_sharpe()
-    cleaned_weights = ef.clean_weights()
 
-    wt = pd.DataFrame(list(cleaned_weights.items()), columns=['Ticker', 'Weight'])
+    wt = pd.DataFrame(list(weights.items()), columns=['Ticker', 'Weight'])
     wt.set_index('Ticker', drop=True, inplace=True)
 
     return wt
@@ -69,18 +65,22 @@ def get_opt_weights(df_price):
 def get_opt_pf_returns(df_price, df_return):
     returns_opt_pf, weights_opt_pf = pd.DataFrame(), pd.DataFrame()
 
+    # Use the trailing 1 year data for mean return and covariance matrix calculations, hence 252 (days)
     for i in range(252, len(df_price)):
-        wt = get_opt_weights(df_price.iloc[i-252:i])
+        prices = df_price.iloc[i - 252:i]
+        wt = get_opt_weights(prices)
         rt = df_return.iloc[i].to_frame('Return')
-        tmp = wt.join(rt, how='left')
-        pf_return = pd.DataFrame({'Return': [sum(tmp['Weight'] * tmp['Return'])]}, index=[df_return.iloc[i].name])
+
+        wt_rt = wt.join(rt, how='left')
+        pf_return = pd.DataFrame({'Return': [sum(wt_rt['Weight'] * wt_rt['Return'])]}, index=[df_return.iloc[i].name])
         returns_opt_pf = pd.concat([returns_opt_pf, pf_return])
-        weights_opt_pf = pd.concat([weights_opt_pf, wt.unstack().unstack()])
+
+        wt = wt.transpose()
+        wt['Date'] = prices.index[-1]
+        wt.set_index('Date', inplace=True)
+        weights_opt_pf = pd.concat([weights_opt_pf, wt])
 
     return returns_opt_pf['Return'], weights_opt_pf
-
-
-
 
 
 
