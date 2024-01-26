a0d5e63f1209e03c1aaf37db570574014869e771
Yanpeng Wu <pengfly20@gmail.com>
2024-01-25 21:24:40 -0500

    Added documentation to data helper functions

--- src/app_utils.py
+++ src/app_utils.py
@@ -1,7 +1,17 @@
 
 def max_drawdown(return_ts):
+    """
+    Helper function to calculate the maximum drawdown for the given price return timeseries.
+
+    Parameters:
+      - return_ts (Series): The daily close-to-close return time series for a single stock.
+
+    Returns:
+      - max_drawdown (float): The maximum drawdown within the time horizon of input series.
+    """
     cum_return_ts = (1 + return_ts).cumprod()[1:]
     high_watermarks = cum_return_ts.cummax()
     drawdowns = 1 - cum_return_ts.div(high_watermarks)
     max_drawdown = - max(drawdowns)
+
     return max_drawdown
